8754aecb19f63ff80d6d27b9dc3a87022343ee6f
Yzwdbwl <2926154221@qq.com>
2024-03-31 23:58:33 +0100

    Todo-list

---
+++ src/main.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+//const DATA = [
+ // { id: "todo-0", name: "Eat", completed: true },
+ // { id: "todo-1", name: "Sleep", completed: false },
+ // { id: "todo-2", name: "Repeat", completed: false },
+//];
+
+const DATA = JSON.parse(localStorage.getItem('tasks')) || [];
+console.log(DATA);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>,
+)
