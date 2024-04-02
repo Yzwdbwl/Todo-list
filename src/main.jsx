e2ec4e050c4b51e4daad1cd74286e09de780769e
Yzwdbwl <2926154221@qq.com>
2024-04-03 00:26:28 +0100

    Todo

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
-//const DATA = [
- // { id: "todo-0", name: "Eat", completed: true },
- // { id: "todo-1", name: "Sleep", completed: false },
- // { id: "todo-2", name: "Repeat", completed: false },
-//];
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+  navigator.serviceWorker
+  .register("./service-worker.js")
+  .then((registration) => {
+  console.log("Service Worker registered! Scope: ", registration.scope);
+  })
+  .catch((err) => {
+  console.log("Service Worker registration failed: ", err);
+  });
+  });
+ }
 
 const DATA = JSON.parse(localStorage.getItem('tasks')) || [];
 console.log(DATA);
